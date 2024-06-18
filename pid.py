e4dd3f3b44a8be04bb7171ea3911367316a41ead
Andre <97815138+andreluong@users.noreply.github.com>
2024-06-17 17:52:33 -0700

    Add better PID functionality

--- pid.py
+++ pid.py
@@ -19,24 +19,12 @@ servoMotor = Motor(Port.B)
 robot = DriveBase(leftMotor, rightMotor, wheel_diameter=55.5, axle_track=104)
 
 # Constants
-DEFAULT_SPEED = 60 # % speed
+DEFAULT_SPEED = 200 # % speed
 COLOR_RANGE = 4 # Range for varying color
 BLUE_RANGE = 5 # Range for varying blue color
 GREEN_RANGE = 7 # Range for varying green color
-DIRECT_COLOR_RANGE = 3 # Range for varying direct color
+DIRECT_COLOR_RANGE = 4 # Range for varying direct color
 SCAN_SPEED = 8 # Speed for rotating robot to scan
-
-# PID
-Kp = 2  # Proportional gain constant (adjust for responsiveness)
-Kd = 0.1  # Derivative gain constant (adjust for stability)
-Ki = 0.01  # Integral gain constant (adjust for smoothness)
-MOTOR_SPEED_LIMIT = 100
-
-# Variables
-previousError = 0
-sumOfErrors = 0
-
-
 
 # Surface thresholds
 SURFACE_GREEN = 42
@@ -45,8 +33,8 @@ SURFACE_BLUE = 57
 # Green thresholds
 GREEN_GREEN = 37
 GREEN_BLUE = 37
-DIRECT_GREEN_GREEN = 34 # 32
-DIRECT_GREEN_BLUE = 15 # 28
+DIRECT_GREEN_GREEN = 33 # 32
+DIRECT_GREEN_BLUE = 14 # 28
 
 # Blue thresholds
 BLUE_GREEN = 30 
@@ -54,6 +42,22 @@ BLUE_BLUE = 48
 DIRECT_BLUE_GREEN = 8 # 13	# 18
 DIRECT_BLUE_BLUE = 25 # 30  # 36
 
+# RED
+# surface 38
+# green-green 22
+# direct-green 8-9
+
+
+
+# PID
+Kp = 6.75 # Proportional gain constant (adjust for responsiveness) 
+Ki = 0.005  # Integral gain constant (adjust for smoothness)
+Kd = 13.5 # Derivative gain constant (adjust for stability)
+MOTOR_SPEED_LIMIT = 250 # mm/s
+
+# Variables
+previousError = 0
+sumOfErrors = 0
 
 
 # Checks if robot is on blue line
@@ -90,19 +94,22 @@ def calculateError():
     print("")
 
     if isonDirectGreenLine(greenValue, blueValue):
-        error = -(30 + DIRECT_GREEN_GREEN - greenValue)
+        # error = (greenValue - DIRECT_GREEN_GREEN) + (blueValue - DIRECT_GREEN_BLUE)
+        error = -42 - (redValue - 8/2)
         print("hit direct green")
     elif isOnDirectBlueLine(greenValue, blueValue):
-        error = -(30 + DIRECT_BLUE_BLUE - blueValue)
+        # error = (greenValue - DIRECT_BLUE_GREEN) + (blueValue - DIRECT_BLUE_BLUE)
+        error = -42 - (redValue - 8/2)
         print("hit direct blue")
     elif (isOnGreenLine(greenValue, blueValue)):
-        error = 0
+        error = redValue - 22
         print("hit green")
     elif (isOnBlueLine(greenValue, blueValue)):
-        error = 0
+        error = redValue - 22
         print("hit blue")
     else:
-        error = 50
+        # error = redValue - 19 # was 19 for target/2
+        error = redValue - 17 # was 19 for target/2
 	
     return error
 
@@ -113,12 +120,38 @@ def calculatePID(error, previousError):
     pidValue = Kp * error + integral + derivative
     return pidValue, integral
 
+
+def stopOnObstacle():
+    leftMotor.run(0)
+    rightMotor.run(0)
+    ev3.speaker.beep(2000, 2000)
+    wait(2000)
+
+# Push obstacle
+def pushObstacle():
+    robot.straight(100)
+    # leftMotor.run_time(DEFAULT_SPEED, 800)
+    # rightMotor.run_time(DEFAULT_SPEED, 800)
+    servoMotor.run_time(-1500, 700, then=Stop.HOLD, wait=True)
+    robot.stop()
+
+# Turn around for ~180 degrees
+def turnAround():
+    robot.turn(180)
+    # leftMotor.run_target(200, 180)
+    # rightMotor.run_target(200, -180)
+    robot.stop()
+
 while True:
-    # if (ultrasonicSensor.distance() <= 100.0):
-        # servoMotor.run_time(-1500, 700, then=Stop.HOLD, wait=True)
-    
-    
     redValue, greenValue, blueValue = colorSensor.rgb()
+    
+    if (ultrasonicSensor.distance() <= 100.0):
+        if isOnGreenLine(greenValue, blueValue):
+            stopOnObstacle()
+            pushObstacle()
+        elif isOnBlueLine(greenValue, blueValue):
+            stopOnObstacle()
+            turnAround()    
      
     error = calculateError()
 
@@ -133,12 +166,11 @@ while True:
     leftSpeed = DEFAULT_SPEED + pidValue
     rightSpeed = DEFAULT_SPEED - pidValue
 
-
     # Ensure speeds are within valid range
     leftSpeed = max(min(leftSpeed, MOTOR_SPEED_LIMIT), -MOTOR_SPEED_LIMIT)
     rightSpeed = max(min(rightSpeed, MOTOR_SPEED_LIMIT), -MOTOR_SPEED_LIMIT)
 
-    # robot.drive(10, 0)
+    # robot.drive(0, 0)
     leftMotor.run(leftSpeed)
     rightMotor.run(rightSpeed)
  
